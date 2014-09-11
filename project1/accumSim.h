f1ee127c66eeb32739d4ab283e205671d2f81012
Lucas <veagatm@gmail.com>
2014-09-11 11:56:39 -0500

    Makefile works now

--- project1/accumSim.h
+++ project1/accumSim.h
@@ -12,7 +12,18 @@ Project 1 - Due 12 September 2014
 #include <cstdlib>
 #include <fstream>
 
-
+/*
+*	These are shared and are defined in util.cpp
+*	to prevent a duplicate symbol clang error
+*/
+extern mem_addr PC;
+extern reg_word R[];
+extern mem_addr TEXT_TOP;
+extern mem_addr DATA_TOP;
+extern mem_addr TOS;
+extern mem_word TEXT_SEG[];
+extern mem_word DATA_SEG[];
+extern mem_word STACK_SEG[];
 
 //Functions
 int init();
