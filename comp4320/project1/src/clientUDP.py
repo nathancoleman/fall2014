be1c2b6c6d320d0c6728a5875eeed0ad22da22e7
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-16 18:26:10 -0500

    Stubbing out ClientUDP

--- comp4320/project1/src/clientUDP.py
+++ comp4320/project1/src/clientUDP.py
@@ -1,1 +1,28 @@
 import socket, struct
+
+class ClientUDP:
+
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.socket.connect((host, port))
+		self.requestID = 0
+
+
+
+	def getVLength(input):
+		# TODO
+		print "TODO: getVLength"
+
+
+
+	def disemvowel(input):
+		# TODO
+		print "TODO: disemvowel"
+
+
+
+if __name__ == "__main__":
+    
+    client = ClientUDP('localhost', 8001)
