dfe686a8a22fa63e42c60396e313908701cf251b
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-16 16:07:35 -0500

    Adding multi-threaded TCP server implementation
    Currently, the server just prints the data that it receives from a client

--- comp4320/project1/src/serverTCP.py
+++ comp4320/project1/src/serverTCP.py
@@ -1,15 +1,37 @@
-import SocketServer
+import socket, SocketServer
 
-HOST 		= 'localhost'
-PORT_NUMBER = 8001
+HOST = 'localhost'
+PORT = 8001
+
+#	ThreadingMixIn allows a new thread to be created
+#	for every request instead of servicing one request
+#	at a time
+
+class MultiThreadTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+	
+	daemon_threads = True
+	allow_reuse_address = True
+
+	def __init__(self, server_address, RequestHandlerClass):
+		SocketServer.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
+
 
 class TCPHandler(SocketServer.BaseRequestHandler):
 
 	def handle(self):
-		print "Handling request"
-		self.data = self.request.recv(1024).strip()
+		self.data = self.request.recv(1024)
+		print "Received: " + str(self.data)
 
-print "Running ServerTCP on " + HOST + ":" + str(PORT_NUMBER) + " (Ctrl + C to quit)"
 
-server = SocketServer.TCPServer((HOST, PORT_NUMBER), TCPHandler)
-server.serve_forever()
+
+if __name__ == "__main__":
+    server = MultiThreadTCPServer((HOST, PORT), TCPHandler)
+    
+    try:
+    	print "Running server at " + HOST + ":" + str(PORT)
+    	print "(Ctrl+C to terminate)"
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        sys.exit(0)
