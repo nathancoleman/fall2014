e9d802ee64bc7437cbb49ea7e2541e9755a9f8ee
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-16 13:41:09 -0500

    Implementing basic python TCP server

--- comp4320/project1/src/serverTCP.py
+++ comp4320/project1/src/serverTCP.py
@@ -1,1 +1,15 @@
-print "Running ServerTCP"
+import SocketServer
+
+HOST 		= 'localhost'
+PORT_NUMBER = 8001
+
+class TCPHandler(SocketServer.BaseRequestHandler):
+
+	def handle(self):
+		print "Handling request"
+		self.data = self.request.recv(1024).strip()
+
+print "Running ServerTCP on " + HOST + ":" + str(PORT_NUMBER) + " (Ctrl + C to quit)"
+
+server = SocketServer.TCPServer((HOST, PORT_NUMBER), TCPHandler)
+server.serve_forever()
