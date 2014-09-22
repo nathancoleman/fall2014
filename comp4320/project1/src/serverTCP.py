3da2a6716cefba7f078ae85eb6a919c50ceb08ab
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-22 08:21:55 -0500

    Cleaning up/adding error checking

--- comp4320/project1/src/serverTCP.py
+++ comp4320/project1/src/serverTCP.py
@@ -17,34 +17,41 @@ class ServerTCP:
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((host, port))
 		self.socket.listen(1)
-		self.conn, self.addr = self.socket.accept()
+		self.client, self.addr = self.socket.accept()
 		print "Client connected from:", self.addr
 		while 1:
 			#1024 max payload + 5 for header
-			data = self.conn.recv(1029).strip()
-			self.process_data(data)
-		conn.close()
+			data = self.client.recv(1029).strip()
+			
+			if data:
+				self.process_data(data)
+			
+		self.client.close()
 
 	def process_data(self, data):
-		pack_format = 'HHB'
-		message_length, request_id, op = struct.unpack(pack_format, data[:5])
-		op = int(chr(op))
-		message = data[5:]
+		try:
+			pack_format = 'HHB'
+			message_length, request_id, op = struct.unpack(pack_format, data[:5])
+			op = int(chr(op))
+			message = data[5:]
 
-		if op == GETVLENGTH:
-			vowel_count = self.get_vowel_count(message)
-			pack_format = 'HHH'
-			message_length = 6
-			packed_data = struct.pack(pack_format, message_length, request_id, vowel_count)
-			self.conn.send(packed_data)
+			if op == GETVLENGTH:
+				vowel_count = self.get_vowel_count(message)
+				pack_format = 'HHH'
+				message_length = 6
+				packed_data = struct.pack(pack_format, message_length, request_id, vowel_count)
+				self.client.send(packed_data)
 
-		elif op == DISEMVOWEL:
-			disemvoweled_message = self.disemvowel(message)
-			pack_format = 'HH'
-			message_length = 4 + len(disemvoweled_message)
-			packed_data = struct.pack(pack_format, message_length, request_id)
-			packed_data = packed_data + disemvoweled_message
-			self.conn.send(packed_data)
+			elif op == DISEMVOWEL:
+				disemvoweled_message = self.disemvowel(message)
+				pack_format = 'HH'
+				message_length = 4 + len(disemvoweled_message)
+				packed_data = struct.pack(pack_format, message_length, request_id)
+				packed_data = packed_data + disemvoweled_message
+				self.client.send(packed_data)
+		
+		except struct.error:
+			print "ERROR PACKING STRUCT"
 
 
 	def get_vowel_count(self, message):
