4334a9872f2f419e1216b1c91ee19b6ab882804e
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-21 20:03:58 -0500

    Adding args and arg checking

--- comp4320/project1/src/clientTCP.c
+++ comp4320/project1/src/clientTCP.c
@@ -1,13 +1,20 @@
 /*
 *	This is the TCP Client for COMP4320 project1
-*	Author: Lucas Saltz
+*	Author: Lucas Saltz/Nathan Coleman
 */
 
 #include <stdio.h>
 #include <sys/socket.h>
 
-int main()
+int main(int argc, char**argv)
 {
-	printf("Running ClientTCP\n");
+	// We should get 3 args w/ argv=[bin/clientTCP, ip, port]
+	// Usage: bin/clientTCP localhost 8001 (i.e.)
+	if (argc != 3)
+	{
+      	printf("ERROR: Expected IP and Port");
+      	exit(1);
+   	}
+
 	return 0;
 }
