31595fa44487d733ebfa17d790fadea863ed7783
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-21 23:23:43 -0500

    Working ServerTCP

--- comp4320/project1/src/serverTCP.py
+++ comp4320/project1/src/serverTCP.py
@@ -6,6 +6,9 @@
 
 import socket, struct, sys
 
+GETVLENGTH = 0x55
+DISEMVOWEL = 0xAA
+
 class ServerTCP:
 
 	def __init__(self, host, port):
@@ -14,22 +17,35 @@ class ServerTCP:
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((host, port))
 		self.socket.listen(1)
-		conn, addr = self.socket.accept()
-		print "Client connected from:", addr
+		self.conn, self.addr = self.socket.accept()
+		print "Client connected from:", self.addr
 		while 1:
 			#1024 max payload + 5 for header
-			data = conn.recv(1029).strip()
+			data = self.conn.recv(1029).strip()
 			self.process_data(data)
 		conn.close()
 
 	def process_data(self, data):
 		pack_format = 'HHB'
-		header = struct.unpack(pack_format, data[:5])
-		header_hex = [hex(x) for x in header]
+		message_length, request_id, op = struct.unpack(pack_format, data[:5])
+		op = int(chr(op))
 		message = data[5:]
 
-		print "Header:", header_hex
-		print "Message:", message
+		if op == GETVLENGTH:
+			vowel_count = self.get_vowel_count(message)
+			pack_format = 'HHH'
+			message_length = 6
+			packed_data = struct.pack(pack_format, message_length, request_id, vowel_count)
+			self.conn.send(packed_data)
+
+		elif op == DISEMVOWEL:
+			disemvoweled_message = self.disemvowel(message)
+			pack_format = 'HH'
+			message_length = 4 + len(disemvoweled_message)
+			packed_data = struct.pack(pack_format, message_length, request_id)
+			packed_data = packed_data + disemvoweled_message
+			self.conn.send(packed_data)
+
 
 	def get_vowel_count(self, message):
 		vowels = "aeiouAEIOU"
@@ -48,10 +64,6 @@ class ServerTCP:
 			message = message.replace(vowel, '')
 
 		return message
-
-	def send_response(self, val):
-		# TODO
-		print "TODO: send_response"
 
 
 
