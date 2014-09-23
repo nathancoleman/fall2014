99e4225c56b5a96c7db2fb3fb97dde89069fb18c
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-09-22 21:20:30 -0500

    Working clientTCP and serverTCP

--- comp4320/project1/src/clientTCP.c
+++ comp4320/project1/src/clientTCP.c
@@ -9,19 +9,45 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+const int GETVLENGTH = 0x55;
+const int DISEMVOWEL = 0xAA;
+char request_id = 1;
+
+struct packed_outgoing
+{
+	uint16_t message_length;
+	uint16_t request_id;
+	uint8_t operation;
+	char message[1024]; // Message not more than 1k
+} __attribute__((__packed__));
+
+struct packed_incoming
+{
+	uint16_t message_length;
+	uint16_t request_id;
+	char message[1024]; // Message not mroe than 1k
+}__attribute__((__packed__));
+
+struct timeval request, response;
+
+typedef struct packed_outgoing packed_out;
+typedef struct packed_incoming packed_in;
 
 int main(int argc, char** argv){
 	
-	/* First is bin/clientTCP, then we need hostname, port, and message */
-	if(argc != 4)
+	/* First is bin/clientTCP, then we need hostname, port, op, and message */
+	if(argc != 5)
 	{
-		printf("Usage: bin/clientTCP host port message");
+		printf("Usage: bin/clientTCP host port message\n");
 		exit(1);
 	}
 
 	char *host = argv[1];
 	int port = atoi(argv[2]);
-	char *message = argv[3];
+	int op = atoi(argv[3]);
+	char *message = argv[4];
 
 	int clientSocket;
 	char buffer[1029];
@@ -37,11 +63,43 @@ int main(int argc, char** argv){
 	addr_size = sizeof serverAddr;
 	
 	if (connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size) < 0)
+	{
 		printf("ERROR CONNECTING\n");
+		exit(1);
+	}
 	else
 		printf("CONNECTED\n");
 
-	sendto(clientSocket, message, strlen(message), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	packed_out *pd = malloc(sizeof(packed_out));
+	pd->message_length = htons(5 + strlen(message));
+	pd->request_id = htons(request_id);
+	pd->operation = op;
+	memcpy(pd->message, message, strlen(message));
+
+	gettimeofday(&request, NULL);
+
+	send(clientSocket, pd, pd->message_length, 0);
+
+	packed_in *pi = malloc(sizeof(packed_in));
+
+	recv(clientSocket, pi, 1029, 0);
+
+	gettimeofday(&response, NULL);
+
+	suseconds_t dt = (response.tv_usec - request.tv_usec);
+
+	if(pd->operation == GETVLENGTH)
+	{
+		uint16_t vowel_count = pi->message[0] << 8;
+		vowel_count = pi->message[1] | vowel_count;
+		printf("Vowel count received: %d\n", vowel_count);
+	}
+	else if(pd->operation == DISEMVOWEL)
+	{
+		printf("Disemvoweled message: %s\n", pi->message);
+	}
+
+	printf("Roundtrip time: %d microseconds\n", dt);
 
 	return 0;
 }
--- comp4320/project1/src/serverTCP.py
+++ comp4320/project1/src/serverTCP.py
@@ -4,7 +4,7 @@
 	Author: Nathan Coleman
 """
 
-import socket, struct, sys
+import socket, struct, sys, binascii
 
 GETVLENGTH = 0x55
 DISEMVOWEL = 0xAA
@@ -17,34 +17,44 @@ class ServerTCP:
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((host, port))
 		self.socket.listen(1)
-		self.client, self.addr = self.socket.accept()
-		print "Client connected from:", self.addr
 		while 1:
-			#1024 max payload + 5 for header
-			data = self.client.recv(1029).strip()
+			try:	
+				print "Waiting for client to connect..."
+				self.client, self.addr = self.socket.accept()
+				print "Client connected from:", self.addr
+				
+				#1024 max payload + 5 for header
+				data = self.client.recv(1029).strip()
+				
+				if data and len(data):
+					self.process_data(data)
 			
-			if data:
-				self.process_data(data)
-			
-		self.client.close()
+			finally:
+				self.client.close()
 
 	def process_data(self, data):
 		try:
-			pack_format = 'HHB'
+			pack_format = '!HHB'
 			message_length, request_id, op = struct.unpack(pack_format, data[:5])
-			op = int(chr(op))
+			op = int(op)
 			message = data[5:]
+			
+			print "Request: {0} {1} {2} {3}".format(message_length, request_id, op, message)
 
 			if op == GETVLENGTH:
+				print "Getting vowel count of", message, "..."
 				vowel_count = self.get_vowel_count(message)
-				pack_format = 'HHH'
+				print "Result:", vowel_count
+				pack_format = '!HHH'
 				message_length = 6
 				packed_data = struct.pack(pack_format, message_length, request_id, vowel_count)
 				self.client.send(packed_data)
 
 			elif op == DISEMVOWEL:
+				print "Disemvoweling", message, "..."
 				disemvoweled_message = self.disemvowel(message)
-				pack_format = 'HH'
+				print "Result:", disemvoweled_message
+				pack_format = '!HH'
 				message_length = 4 + len(disemvoweled_message)
 				packed_data = struct.pack(pack_format, message_length, request_id)
 				packed_data = packed_data + disemvoweled_message
