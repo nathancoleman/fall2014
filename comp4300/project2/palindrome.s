44ada898b4cb46b46ae60857dcc9bf42bb15b839
Lucas <veagatm@gmail.com>
2014-10-02 22:37:43 -0500

    Edited register names

--- comp4300/project2/palindrome.s
+++ comp4300/project2/palindrome.s
@@ -6,48 +6,48 @@ string_space: .space 1024
 .text
 main:
 
-	la $a0, string_space
-	li $a1, 1024
-	li $v0, 8
+	la $10, string_space
+	li $11, 1024
+	li $20, 8
 	syscall
 
-	la $t1, string_space
-	la $t2, string_space
+	la $01, string_space
+	la $02, string_space
 
 length_loop:
-	lb $t3, ($t2)
-	beqz $t3, end_length_loop
+	lb $03, ($02)
+	beqz $03, end_length_loop
 
-	addi $t2, $t2, 1
+	addi $2, $2, 1
 	b length_loop
 
 end_length_loop:
-	subi $t2, $t2, 2
+	subi $2, $2, 2
 
 test_loop:
-	bge $t1, $t2, is_palin
+	bge $01, $2, is_palin
 
-	lb $t3, ($t1)
-	lb $t4, ($t2)
+	lb $t3, ($1)
+	lb $t4, ($2)
 	bne $t3, $t4, not_palin
 
-	addi $t1, $t1, 1
-	subi $t2, $t2, 1
+	addi $1, $t1, 1
+	subi $2, $2, 1
 	b test_loop
 
 is_palin:
 
-	la $a0, is_palin_msg
-	li $v0, 4
+	la $10, is_palin_msg
+	li $20, 4
 	syscall
 	b exit
 
 not_palin:
-	la $a0, not_palin_msg
-	li $v0, 4
+	la $10, not_palin_msg
+	li $20, 4
 	syscall
 	b exit
 
 exit:
-	li $v0, 10
+	li $20, 10
 	syscall
