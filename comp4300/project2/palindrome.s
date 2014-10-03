74d507f9a6b92a9b38fca80a51c9ae2dd53db817
Lucas <veagatm@gmail.com>
2014-10-03 02:16:52 -0500

    Arranged input file to suit parsing needs

--- comp4300/project2/palindrome.s
+++ comp4300/project2/palindrome.s
@@ -6,48 +6,48 @@ string_space: .space 1024
 .text
 main:
 
-	la $10, string_space
-	li $11, 1024
-	li $20, 8
-	syscall
+la $10, string_space
+li $11, 1024
+li $20, 8
+syscall
 
-	la $01, string_space
-	la $02, string_space
+la $01, string_space
+la $02, string_space
 
 length_loop:
-	lb $03, ($02)
-	beqz $03, end_length_loop
+lb $03, ($02)
+beqz $03, end_length_loop
 
-	addi $2, $2, 1
-	b length_loop
+addi $2, $2, 1
+b length_loop
 
 end_length_loop:
-	subi $2, $2, 2
+subi $2, $2, 2
 
 test_loop:
-	bge $01, $2, is_palin
+bge $01, $2, is_palin
 
-	lb $3, ($1)
-	lb $4, ($2)
-	bne $3, $4, not_palin
+lb $3, ($1)
+lb $4, ($2)
+bne $3, $4, not_palin
 
-	addi $1, $1, 1
-	subi $2, $2, 1
-	b test_loop
+addi $1, $1, 1
+subi $2, $2, 1
+b test_loop
 
 is_palin:
 
-	la $10, is_palin_msg
-	li $20, 4
-	syscall
-	b exit
+la $10, is_palin_msg
+li $20, 4
+syscall
+b exit
 
 not_palin:
-	la $10, not_palin_msg
-	li $20, 4
-	syscall
-	b exit
+la $10, not_palin_msg
+li $20, 4
+syscall
+b exit
 
 exit:
-	li $20, 10
-	syscall
+li $20, 10
+syscall
