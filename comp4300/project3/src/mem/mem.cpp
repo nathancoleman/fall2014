a79fe0a17958e2768e29db9a950fbf872bc8126a
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-10-09 20:47:39 -0500

    Cleaning up

--- comp4300/project3/src/mem/mem.cpp
+++ comp4300/project3/src/mem/mem.cpp
@@ -9,10 +9,10 @@ mem_word TEXT_SEG[TEXT_SEG_LENGTH];
 
 mem_word read(mem_address address)
 {
-	mem_address local_address = address - DATA_SEG_BASE;
-
 	if ((address > DATA_SEG_BASE) && (address < TEXT_SEG_BASE))
 	{
+		mem_address local_address = address - DATA_SEG_BASE;
+		
 		if (local_address > DATA_SEG_LENGTH)
 			throw std::runtime_error("*** MEMORY ERROR *** : Address outside data space");
 
