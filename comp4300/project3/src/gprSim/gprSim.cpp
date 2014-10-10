8ccd9d5fe87ba36d82b70f856e81d51409f364f3
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-10-09 21:13:18 -0500

    WIP: Rewrite

--- comp4300/project3/src/gprSim/gprSim.cpp
+++ comp4300/project3/src/gprSim/gprSim.cpp
@@ -1,10 +1,25 @@
 #include "gprSim.h"
 
-int main()
+/* Initialize externs */
+std::string FILENAME;
+
+int main(int argc, char* argv[])
 {
+	if (argc > 1)
+		FILENAME = argv[1];
+	else
+		throw std::runtime_error("*** RUNTIME ERROR *** : Filename expected");
+
+	init();
+
 	execute();
 
 	return 0;
+}
+
+void init()
+{
+	init_regs();
 }
 
 void execute()
--- comp4300/project3/src/gprSim/gprSim.h
+++ comp4300/project3/src/gprSim/gprSim.h
@@ -1,4 +1,21 @@
 #include "../mem/mem.h"
+#include "../reg/reg.h"
 #include <stdio.h>
+#include <stdexcept>
+#include <string>
 
+#define ADDI	0x00 //	Add Immediate
+#define B 		0x01 //	Branch to target
+#define BEQZ	0x02 // Branch on >= 0
+#define BGE		0x03 //	Branch on >=
+#define BNE		0x04 // Branch on Not Equal
+#define LA		0x05 // Load Address
+#define LB		0x06 // Load Byte
+#define LI 		0x07 // Load Immediate
+#define SUBI	0x08 // Subtract Immediate
+#define SYSCALL	0x09 // System Call
+
+extern std::string FILENAME;
+
+void init();
 void execute();
