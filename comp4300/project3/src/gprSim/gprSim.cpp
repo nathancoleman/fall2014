99a57ec351a2f2e531ad2d81399587c1f9f5ca16
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-10-27 00:22:21 -0500

    WIP: project3

--- comp4300/project3/src/gprSim/gprSim.cpp
+++ comp4300/project3/src/gprSim/gprSim.cpp
@@ -33,6 +33,7 @@ void run()
 	ex_mem_latch ex_mem;
 	mem_wb_latch mem_wb;
 	bool run = true;
+	int run_times = 0;
 
 	printf("Executing...\n");
 
@@ -51,6 +52,11 @@ void run()
 			run = false;
 			printf("\tHALTING EXECUTION\n");
 		}
+
+		if (run_times > 250)
+			run = false;
+		else
+			run_times++;
 	}
 
 	printf("Execution complete!\n");
@@ -83,25 +89,26 @@ id_ex_latch instr_decode(if_id_latch if_id)
 				break;
 
 			case BEQZ:
-				id_ex.rt = get_rt(if_id.ir);
+				id_ex.rd = get_rd(if_id.ir);
+				id_ex.operand_A = R[id_ex.rd];
 				id_ex.new_PC = TEXT_SEG_BASE + get_imm(if_id.ir);
 				printf("\t\tBEQZ %x\n", id_ex.new_PC);
 				break;
 
 			case BGE:
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.rt = get_rt(if_id.ir);
-				id_ex.rs = get_rs(if_id.ir);
-				id_ex.operand_A = R[id_ex.rt];
-				id_ex.operand_B = R[id_ex.rs];
+				id_ex.operand_A = R[id_ex.rd];
+				id_ex.operand_B = R[id_ex.rt];
 				id_ex.new_PC = TEXT_SEG_BASE + get_imm(if_id.ir);
 				printf("\t\tBGE %x\n", id_ex.new_PC);
 				break;
 
 			case BNE:
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.rt = get_rt(if_id.ir);
-				id_ex.rs = get_rs(if_id.ir);
-				id_ex.operand_A = R[id_ex.rt];
-				id_ex.operand_B = R[id_ex.rs];
+				id_ex.operand_A = R[id_ex.rd];
+				id_ex.operand_B = R[id_ex.rt];
 				id_ex.new_PC = TEXT_SEG_BASE + get_imm(if_id.ir);
 				printf("\t\tBNE %x\n", id_ex.new_PC);
 				break;
@@ -135,30 +142,31 @@ id_ex_latch instr_decode(if_id_latch if_id)
 				break;
 
 			case LA:
-				id_ex.rt = get_rt(if_id.ir);
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.imm_offset = get_imm(if_id.ir);
-				printf("\t\tLA $%x, %x\n", id_ex.rt, id_ex.imm_offset);
+				printf("\t\tLA $%d, %x\n", id_ex.rd, id_ex.imm_offset);
 				break;
 
 			case LB:
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.rt = get_rt(if_id.ir);
-				id_ex.rs = get_rs(if_id.ir);
-				id_ex.operand_A = R[id_ex.rs];
-				printf("\t\tLB $%x, %x", id_ex.rt, id_ex.operand_A);
+				id_ex.imm_offset = get_imm(if_id.ir);
+				id_ex.operand_A = R[id_ex.rt];
+				printf("\t\tLB $%d, $%d (Address: %x)\n", id_ex.rd, id_ex.rt, id_ex.operand_A);
 				break;
 
 			case LI:
-				id_ex.rt = get_rt(if_id.ir);
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.imm_offset = get_imm(if_id.ir);
-				printf("\t\tLI $%x, %d\n", id_ex.rt, id_ex.imm_offset);
+				printf("\t\tLI $%x, %d\n", id_ex.rd, id_ex.imm_offset);
 				break;
 
 			case SUBI:
+				id_ex.rd = get_rd(if_id.ir);
 				id_ex.rt = get_rt(if_id.ir);
-				id_ex.rs = get_rs(if_id.ir);
-				id_ex.operand_A = R[id_ex.rs];
+				id_ex.operand_A = R[id_ex.rt];
 				id_ex.imm_offset = get_imm(if_id.ir);
-				printf("\t\tSUBI $%x, %d, %d\n", id_ex.rt, id_ex.operand_A, id_ex.imm_offset);
+				printf("\t\tSUBI $%x, %d, %d\n", id_ex.rd, id_ex.operand_A, id_ex.imm_offset);
 				break;
 
 			case SYSCALL:
@@ -187,34 +195,92 @@ ex_mem_latch instr_execute(id_ex_latch id_ex)
 
 	ex_mem_latch ex_mem;
 	ex_mem.op_code = id_ex.op_code;
+	ex_mem.operand_B = id_ex.operand_B;
+	ex_mem.rd = id_ex.rd;
 
-	switch (ex_mem.op_code)
+	if (is_branch_instr(ex_mem.op_code))
 	{
-		case ADDI:
-			break;
+		switch (ex_mem.op_code)
+		{
+			case B:
+				printf("\t\tBranching...\n");
+				PC = id_ex.new_PC - 1;
+				ex_mem.op_code = NOP;
+				break;
 
-		case LA:
-			break;
+			case BEQZ:
+				if (id_ex.operand_A == 0)
+				{
+					printf("\t\tBranching...\n");
+					PC = id_ex.new_PC - 1;
+					ex_mem.op_code = NOP;
+				}
+				break;
 
-		case LB:
-			break;
+			case BGE:
+				if ((int)id_ex.operand_A >= (int)id_ex.operand_B)
+				{
+					printf("\t\tBranching...\n");
+					PC = id_ex.new_PC - 1;
+					ex_mem.op_code = NOP;
+				}
+				break;
 
-		case LI:
-			break;
+			case BNE:
+				if (id_ex.operand_A != id_ex.operand_B)
+				{
+					printf("\t\tBranching...\n");
+					PC = id_ex.new_PC - 1;
+					ex_mem.op_code = NOP;
+				}
+				break;
 
-		case SUBI:
-			break;
+			default:
+				// This is an invalid op code
+				// Do nothing
+				break;
+		}
+	}
 
-		case SYSCALL:
-			break;
+	else
+	{
+		switch (ex_mem.op_code)
+		{
+			case ADD:
+				ex_mem.alu_out = id_ex.operand_A + id_ex.operand_B;
+				break;
 
-		case NOP:
-			break;
+			case ADDI:
+				ex_mem.alu_out = id_ex.operand_A + id_ex.imm_offset;
+				break;
 
-		default:
-			// This is an invalid op code
-			// Do nothing
-			break;
+			case LA:
+				ex_mem.alu_out = id_ex.imm_offset;
+				break;
+
+			case LB:
+				ex_mem.alu_out = id_ex.operand_A;
+				break;
+
+			case LI:
+				ex_mem.alu_out = id_ex.imm_offset;
+				break;
+
+			case SUBI:
+				ex_mem.alu_out = id_ex.operand_A - id_ex.imm_offset;
+				break;
+
+			case SYSCALL:
+				break;
+
+			case NOP:
+				break;
+
+			default:
+				// This is an invalid op code
+				// Do nothing
+				break;
+		}
 	}
 
 	return ex_mem;
@@ -225,6 +291,17 @@ mem_wb_latch mem_access(ex_mem_latch ex_mem)
 	printf("\tAccessing memory...\n");
 
 	mem_wb_latch mem_wb;
+	mem_wb.op_code = ex_mem.op_code;
+	mem_wb.alu_out = ex_mem.alu_out;
+	mem_wb.rd = ex_mem.rd;
+	mem_wb.operand_B = ex_mem.operand_B;
+
+	switch (mem_wb.op_code)
+	{
+		case LB:
+			mem_wb.mdr = read_mem(ex_mem.alu_out) >> 24; // First 8 bits is byte
+			break;
+	}
 
 	return mem_wb;
 }
@@ -232,6 +309,33 @@ mem_wb_latch mem_access(ex_mem_latch ex_mem)
 void write_back(mem_wb_latch mem_wb)
 {
 	printf("\tWriting back...\n");
+
+	switch (mem_wb.op_code)
+	{
+		case ADD:
+			R[mem_wb.rd] = mem_wb.alu_out;
+			break;
+
+		case ADDI:
+			R[mem_wb.rd] = mem_wb.alu_out;
+			break;
+
+		case LA:
+			R[mem_wb.rd] = mem_wb.alu_out;
+			break;
+
+		case LB:
+			printf("\t\tWriting %d to $%d\n", mem_wb.mdr, mem_wb.rd);
+			R[mem_wb.rd] = mem_wb.mdr;
+			break;
+
+		case LI:
+			R[mem_wb.rd] = mem_wb.alu_out;
+			break;
+
+		case SUBI:
+			R[mem_wb.rd] = mem_wb.alu_out;
+	}
 }
 
 void update_PC()
