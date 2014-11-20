5fd4f447f1821bfd6e5c600eab1c270f689696b4
Lucas <veagatm@gmail.com>
2014-11-19 18:32:09 -0600

    removed unnecesary code

--- comp4320/project3/src/Server.c
+++ comp4320/project3/src/Server.c
@@ -31,97 +31,6 @@ void error(char *msg) {
   exit(1);
 }
 
-/*
-This function counts the number of vowels in the buffer
-*/
-short vowelNumber(char* wordBuffer, size_t size) {
-	short vowelCount = 0;
-	unsigned int i;
-
-	for (i = 0; i < size; i++) 
-	{
-		switch(wordBuffer[i]) {
-			case 'a':
-				vowelCount += 1;
-				break;
-			case 'A':
-				vowelCount += 1;
-				break;
-			case 'e':
-				vowelCount += 1;
-				break;
-			case 'E':
-				vowelCount += 1;
-				break;
-			case 'i':
-				vowelCount += 1;
-				break;
-			case 'I':
-				vowelCount += 1;
-				break;
-			case 'o':
-				vowelCount += 1;
-				break;
-			case 'O':
-				vowelCount += 1;
-				break;
-			case 'u':
-				vowelCount += 1;
-				break;
-			case 'U':
-				vowelCount += 1;
-				break;
-		}
-	}
-	return vowelCount;
-}
-
-int isaVowel(char a) 
-{
-	switch(a)
-	{
-		case 'a':
-			return TRUE;
-		case 'A':
-			return TRUE;
-		case 'e':
-			return TRUE;
-		case 'E':
-			return TRUE;
-		case 'i':
-			return TRUE;
-		case 'I':
-			return TRUE;
-		case 'o':
-			return TRUE;
-		case 'O':
-			return TRUE;
-		case 'u':
-			return TRUE;
-		case 'U':
-			return TRUE;
-		default:
-			return FALSE;
-	}
-	return FALSE;
-}
-
-char* disemVoweling(char* wordBuffer, size_t size) 
-{
-	char* p = malloc(size - vowelNumber(wordBuffer, size)); //i +size??
-	unsigned int i, j=0;
-
-	for(i=0; i < size; i++)
-	{
-		if (isaVowel(wordBuffer[i]) == FALSE) //removing char if not a vowel
-		{
-			p[j] = wordBuffer[i];
-			j += 1;
-		}
-	}
-	p[j] = '\0'; //taking off last part of the array?
-	return p;
-}
 
 
 int main(int argc, char **argv) {
@@ -207,9 +116,7 @@ int main(int argc, char **argv) {
 		Starting new code not from example code
   	********************************************************/
   	char operation = buf[4];
-  	//print out the operation
-  	//printf("Operation recieved: ");
-  	//printf("%i\n", operation);
+
 
 
   	if(operation == 0x55) //0x55 is hex for vLength
@@ -222,7 +129,7 @@ int main(int argc, char **argv) {
   		{
   			buffer[i] = buf[i+5];
   		}
-  		short numVowels = vowelNumber(buffer, sizeof(buffer));
+  		//short numVowels = vowelNumber(buffer, sizeof(buffer));
   		char response[6];
   		short length = (short) sizeof(response);
   	/*******************************************************
@@ -232,8 +139,7 @@ int main(int argc, char **argv) {
 		response[1] = (char)(length & 0xff); //taking just the end
 		response[2] = buf[2];
 		response[3] = buf[3];
-		response[4] = (char)(numVowels >> 8);
-		response[5] = (char)(numVowels & 0xff);
+
 		n = sendto(sockfd, response, length, 0, 
 	       (struct sockaddr *) &clientaddr, clientlen);
 		printf("sent response back\n");
@@ -250,20 +156,9 @@ int main(int argc, char **argv) {
   		{
   			buffer[i] = buf[i+5];
   		}
-  		char* stringV = disemVoweling(buffer, sizeof(buffer));
-  		short length = (short)(strlen(stringV) + 4);
-  		char byteMessage[length];
-  		byteMessage[0] = (char)(length >> 8);
-  		byteMessage[1] = (char)(length & 0xff);
-  		byteMessage[2] = buf[2];
-  		byteMessage[3] = buf[3];
 
-  		for (i = 0; i < (length - 4); i++) //i++???
-  		{
-  			byteMessage[i+4] = stringV[i];
-  		}
-  		n = sendto(sockfd, byteMessage, length, 0, 
-	       (struct sockaddr *) &clientaddr, clientlen);
+  		//n = sendto(sockfd, byteMessage, length, 0, 
+	      // (struct sockaddr *) &clientaddr, clientlen);
 		printf("sent back to client\n");
     	if (n < 0) 
       		error("ERROR in sendto");
