a056d18c14156c95d40b52525b9d28a1c3d55bc2
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-12-03 21:32:10 -0600

    Fixing input file formatting

--- comp4300/project4/lab4b.s
+++ comp4300/project4/lab4b.s
@@ -1,21 +1,23 @@
+.text
 
-	.text
+main:
+li $1, 0
+li $2, 10
+li $3, 100
+li $4, 1000
+li $5, 7
 
-main:	li $5, 7
-	li $f0, 0
-	li $f1, 10
-	li $f3, 100
-	li $f4, 1000
-	
+loop:
+subi $5, $5, 1
+add $1, $1, $2
+add $1, $1, $3
+add $1, $1, $4
+bge  $5, $0, loop
+nop
 
-loop:	subi $5, $5, 1
-	fadd $f0, $f0, $f1
-	fadd $f0, $f0, $f3
-	fadd $f0, $f0, $f4
-	bge  $5, $0, loop
-	nop
-	nop
+add $4, $0, $1
+li $2, 1
+syscall
 
-	li $2, 10
-	syscall
-	
+li $2, 10
+syscall
