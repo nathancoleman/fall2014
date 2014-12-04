619a3000ee2d10ab3bdd7bb7c37c0ce4a6ff90ac
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-12-03 20:08:22 -0600

    Updating Makefile

---
+++ comp4300/project4/src/scoreboard/scoreboard.cpp
@@ -0,0 +1,6 @@
+#include "scoreboard.h"
+
+int main()
+{
+	return 0;
+}
---
+++ comp4300/project4/src/scoreboard/scoreboard.h
@@ -0,0 +1,7 @@
+#include "../mem/mem.h"
+#include "../reg/reg.h"
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <stdexcept>
+#include <string>
