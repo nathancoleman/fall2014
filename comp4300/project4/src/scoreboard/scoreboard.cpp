5eeec08bdea6da637ad0c698e7959249a5f44ab2
Nathan Coleman <nathandanielcoleman@gmail.com>
2014-12-03 22:04:58 -0600

    WIP: project4

--- comp4300/project4/src/scoreboard/scoreboard.cpp
+++ comp4300/project4/src/scoreboard/scoreboard.cpp
@@ -1,1 +1,57 @@
 #include "scoreboard.h"
+
+string FILENAME;
+unsigned int cycleCount = 1;
+
+int main(int argc, char* argv[])
+{
+	if (argc > 1)
+		FILENAME = argv[1];
+	else
+		throw std::runtime_error("*** RUNTIME ERROR *** : Filename expected");
+
+	init();
+
+	run();
+
+	return 0;
+}
+
+void init()
+{
+	init_regs();
+
+	init_segs(FILENAME);
+
+	init_instr_table();
+}
+
+void run()
+{
+	printf("Executing...\n");
+
+	if (can_issue_instr())
+		issue_instr();
+}
+
+bool can_issue_instr()
+{
+	return true;
+}
+
+void issue_instr()
+{
+	instruction next_instr = read_mem(PC);
+	instr_status[PC-TEXT_SEG_BASE][ISSUE] = cycleCount;
+
+	print_instr_status();
+}
+
+void print_instr_status()
+{
+	int arraySize = TEXT_TOP - TEXT_SEG_BASE;
+
+	int i;
+	for (i = 0; i < arraySize; i++)
+		printf("%d %d %d %d\n", instr_status[i][ISSUE], instr_status[i][READ_OPS], instr_status[i][EXEC_COMP], instr_status[i][WRITE_RES]);
+}
--- comp4300/project4/src/scoreboard/scoreboard.h
+++ comp4300/project4/src/scoreboard/scoreboard.h
@@ -1,10 +1,45 @@
-#include "../mem/mem.h"
 #include "../reg/reg.h"
+#include "../mem/mem.h"
+#include "../util/util.h"
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 #include <stdexcept>
 #include <string>
 
-int cycleCount, nopCount, instructionCount;
+using namespace std;
 
+extern string FILENAME;
+
+#define ISSUE 		0
+#define READ_OPS	1
+#define EXEC_COMP	2
+#define WRITE_RES	3
+
+#define INTEGER_FU	0
+#define MULT1_FU 	1
+#define MULT2_FU	2
+#define ADD_FU		3
+#define DIVIDE_FU	4
+
+#define BUSY 		0
+#define OP 		 	1
+#define FI 		 	2
+#define FJ 		 	3
+#define FK 		 	4
+#define QJ 		 	5
+#define QK 		 	6
+#define RJ 		 	7
+#define RK 		 	8
+
+unsigned int instr_status[TEXT_SEG_LENGTH][4];
+unsigned int fu_status[5][9];
+unsigned int res_status[F_LENGTH];
+
+void init();
+void run();
+
+bool can_issue_instr();
+void issue_instr();
+
+void print_instr_status();
