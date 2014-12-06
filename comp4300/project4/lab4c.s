1f718c2e503fc4dc5723d776517736fa28d0ae22
Lucas <veagatm@gmail.com>
2014-12-06 12:46:34 -0600

    corrected lab4c.s

--- comp4300/project4/lab4c.s
+++ comp4300/project4/lab4c.s
@@ -1,35 +1,35 @@
 .data
 d32: .float 1.65
 d34: .float -0.57
-df17: .float 0.0
-df19: .float 0.80
-df20: .float 0.0
-df21: .float -0.92
+d33: .float 0.0
+d35: .float 0.80
+d36: .float 0.0
+d37: .float -0.92
 
 .text
 main:
-ld $f16, d32
-ld $f18, d34
-ld $f17, d17
-ld $f19, d19
-ld $f21, d21
-ld $f20, d36
+ld $f0, d32
+ld $f2, d34
+ld $f1, d33
+ld $f3, d35
+ld $f5, d37
+ld $f4, d36
 li $1, 0
 li $2, 99
 
 loop:
 nop
 nop
-fsub $f24, $f17, $f18
-fmul $f22, $f16, $f24
-fsub $f24, $f20, $f21
-fmul $f23, $f19, $f24
-ld $f18, $f17
-ld $f17, $f23 
-ld $f21, $f20 
-ld $f20, $f23
-sd $f22
-sd $f23
+fsub $f8, $f1, $f2
+fmul $f6, $f0, $f8
+fsub $f8, $f4, $f5
+fmul $f7, $f3, $f8
+ld $f2, $f1
+ld $f1, $f7 
+ld $f5, $f4 
+ld $f4, $f7
+sd $f6
+sd $f7
 add $1, $1, 1
 bne $5, 0 loop
 nop
