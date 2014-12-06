e3658635f1cc162a7755894f593822c4828b4525
Lucas <veagatm@gmail.com>
2014-12-06 12:36:43 -0600

    Changed lab4.c to work with ld instruction - need
    need to now make ld function similarly to la

--- comp4300/project4/lab4c.s
+++ comp4300/project4/lab4c.s
@@ -1,27 +1,35 @@
+.data
+d32: .float 1.65
+d34: .float -0.57
+df17: .float 0.0
+df19: .float 0.80
+df20: .float 0.0
+df21: .float -0.92
+
 .text
 main:
-ld $32, 1.65
-ld $34, -0.57
-ld $33, 0.0
-ld $35, 0.80
-ld $37, -0.92
-ld $36, 0.0
-ld $1, 0
-ld $2, 99
+ld $f16, d32
+ld $f18, d34
+ld $f17, df17
+ld $f19, df19
+ld $f21, df21
+ld $f20, d36
+li $1, 0
+li $2, 99
 
 loop:
 nop
 nop
-fsub $40, $33, $34
-fmul $38, $32, $40
-fsub $40, $36, $37
-fmul $39, $35, $40
-ld $34, $33
-ld $33, $39 
-ld $37, $36 
-ld $36, $39
-sd $38
-sd $39
+fsub $f24, $f17, $f18
+fmul $f22, $f16, $f24
+fsub $f24, $f20, $f21
+fmul $f23, $f19, $f24
+ld $f18, $f17
+ld $f17, $f23 
+ld $f21, $f20 
+ld $f20, $f23
+sd $f22
+sd $f23
 add $1, $1, 1
 bne $5, 0 loop
 nop
